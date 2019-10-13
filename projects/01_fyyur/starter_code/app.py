e94a67a0c5de3d39393dfa532843cbe0f83f0f7a
Matt Rhone <3081486+kritoke@users.noreply.github.com>
2019-10-13 14:17:35 -0500

    Add edit artist.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -341,27 +341,26 @@ def show_artist(artist_id):
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
   form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
+  artist = Artist.query.get(artist_id)
+  form.state.process_data(artist.state)
+  form.genres.process_data(artist.genres)
   # TODO: populate form with fields from artist with ID <artist_id>
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-  # TODO: take values from the form submitted, and update existing
-  # artist record with ID <artist_id> using the new attributes
-
+  artist = Artist.query.get(artist_id)
+  artist.name = request.form.get('name')
+  artist.city = request.form.get('city')
+  artist.state = request.form.get('state')
+  artist.phone = request.form.get('phone')
+  artist.genres = request.form.getlist('genres')
+  artist.image_link = request.form.get('image_link')
+  artist.facebook_link = request.form.get('facebook_link')
+  artist.website = request.form.get('website')
+  artist.seeking_venue = request.form.get('seeking_venue')
+  artist.seeking_description = request.form.get('seeking_description')
+  db.session.commit()
   return redirect(url_for('show_artist', artist_id=artist_id))
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
